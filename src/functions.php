69c6ab1f0bb8551135cfc8776363ef3f21fca59f
Colin <colin1124x@gmail.com>
2014-09-11 12:31:11 +0800

    加入新陣列操作函式

--- src/functions.php
+++ src/functions.php
@@ -2,9 +2,16 @@
 
 namespace Rde;
 
+/** @todo 雖然已經在composer指定5.4+,但暫時還是保留5.3相容寫法 */
+
+function is_array($arg)
+{
+    return  \is_array($arg) || \is_a($arg, 'ArrayAccess');
+}
+
 function array_get($arr, $key, $default = null)
 {
-    if ( ! is_array($arr) && ! is_a($arr, 'ArrayAccess')) {
+    if ( ! is_array($arr)) {
         throw new \InvalidArgumentException('參數1必須為陣列或ArrayAccess實體');
     }
 
@@ -17,6 +24,20 @@ function array_get($arr, $key, $default = null)
     }
 
     return $default;
+}
+
+function array_each(callable $callback)
+{
+    foreach (array_slice(func_get_args(), 1) as $i => $arr) {
+        if ( ! is_array($arr)) {
+            throw new \InvalidArgumentException("參數".($i+2)."必須為陣列或ArrayAccess實體");
+        }
+        foreach ($arr as $k => $v) {
+            if (false === $callback($k, $v)) {
+                break;
+            }
+        }
+    }
 }
 
 // debug
--- tests/ArrayFunctionsTest.php
+++ tests/ArrayFunctionsTest.php
@@ -1,10 +1,24 @@
 <?php
 
+class FakeArray implements ArrayAccess
+{
+    public function offsetExists($i){}
+    public function offsetGet($i){}
+    public function offsetSet($i, $v){}
+    public function offsetUnset($i){}
+}
 /**
  * @group array
  */
 class ArrayFunctionTest extends PHPUnit_Framework_TestCase
 {
+    public function testIsArray()
+    {
+        $this->assertTrue(Rde\is_array([]));
+        $this->assertTrue(Rde\is_array(new FakeArray()));
+        $this->assertFalse(Rde\is_array(null));
+    }
+
     public function testArrayGet()
     {
         $arr = array('a' => 1);
@@ -21,5 +35,24 @@ class ArrayFunctionTest extends PHPUnit_Framework_TestCase
     public function testArrayGetException()
     {
         \Rde\array_get(null, 'a');
+    }
+
+    public function testArrayEach()
+    {
+        $tester = $this;
+        $tmp1 = array('a' => 9, 'b' => 5);
+        $tmp2 = array('x' => 1, 'y' => 2);
+        $tmp3 = array();
+
+        Rde\array_each(function($k, $v) use($tester, &$tmp3) {
+            $tmp3[] = array($k, $v);
+
+            'a' === $k and $tester->assertEquals(9, $v);
+            'b' === $k and $tester->assertEquals(5, $v);
+            'x' === $k and $tester->assertEquals(1, $v);
+            'y' === $k and $tester->assertEquals(2, $v);
+        }, $tmp1, $tmp2);
+
+        $tester->assertEquals(4, count($tmp3));
     }
 }
